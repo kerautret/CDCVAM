fa2677f74442da8fd309a03ed1f4b249f914cb9b
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2021-07-05 15:56:42 +0200

    CLI11 in statisticsCentricity

--- tools/statisticsCentricity.cpp
+++ tools/statisticsCentricity.cpp
@@ -13,12 +13,6 @@
 #include <DGtal/kernel/sets/DigitalSetBySTLSet.h>
 #include "DGtal/geometry/volumes/distance/FMM.h"
 
-
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/parsers.hpp>
-#include <boost/program_options/variables_map.hpp>
-
-
 #include "DGtal/topology/KhalimskySpaceND.h"
 #include "DGtal/topology/helpers/Surfaces.h"
 
@@ -26,12 +20,12 @@
 
 #include "NormalAccumulator.h"
 
-
+#include "CLI11.hpp"
 
 
 using namespace std;
 using namespace DGtal;
-namespace po = boost::program_options;
+
 
 
 typedef ImageContainerBySTLVector<Z3i::Domain, bool> Image3DMaker;
@@ -48,61 +42,40 @@ typedef FMM<DistanceImage, AcceptedPointSet, Z3i::DigitalSet, DistanceMeasure> T
 
 
 
-
-
-
-
 /**
  * @brief main function call
  *
  */
 int main(int argc, char *const *argv)
 {
-  po::options_description general_opt("Allowed options are: ");
-  general_opt.add_options()
-    ("help,h", "display this message")
-    ("input,i", po::value<std::string>(), "input mesh.")
-    ("output,o", po::value<std::string>()->default_value("result"), "the output base name.")
-    ("invertNormal,n", "invert normal to apply accumulation.")
-    ("radiusCompare,r", po::value<double>()->default_value(10.0), "reference Radius to apply comparisons.")
-    ("confidenceTh,c", po::value<double>()->default_value(0.5), "threshold confidence image to apply stat.")
-    ("accumulationTh,a", po::value<double>()->default_value(0.5), "threshold accumulation image to apply stat.")
-    ("radiusComputeAcc,d", po::value<double>()->default_value(10.0), "distance (d_{acc}) used to compute the accumulation.");
-  
-
-  
-  
-  bool parseOK = true;
-  po::variables_map vm;
-  try
-  {
-    po::store(po::parse_command_line(argc, argv, general_opt), vm);
-  }
-  catch (const std::exception &ex)
-  {
-    trace.info() << "Error checking program options: " << ex.what() << std::endl;
-    parseOK = false;
-  }
-  po::notify(vm);
-  if ( !parseOK || vm.count("help") || argc <= 1 || !vm.count("input") )
-  {
-    trace.info() << "Computes centricity statistics obtained on confidence and accumulation estimation." << std::endl
-                 << "Options: " << std::endl
-                 << general_opt << std::endl;
-    return 0;
-  }
-
-
   // Reading parameters:
-  std::string inputMeshName = vm["input"].as<std::string>();
-  std::string outputName = vm["output"].as<std::string>();
-  bool invertNormal = vm.count("invertNormal");
-  double radiusCompare = vm["radiusCompare"].as<double>();
-  double radiusComputeAcc = vm["radiusComputeAcc"].as<double>();
-  double confidenceTh = vm["confidenceTh"].as<double>();
-  double accumulationTh = vm["accumulationTh"].as<double>();
+  std::string inputMeshName ;
+  std::string outputName {"result"};
+  bool invertNormal {false};
 
+  double radiusCompare {10.0};
+  double radiusComputeAcc {10.0};
+
+  double confidenceTh {0.5};
+  double accumulationTh {0.5};
   
+  CLI::App app;
+  app.description("Computes centricity statistics obtained on confidence and accumulation estimation.");
+  app.add_option("-i,--input,1", inputMeshName, "input mesh" )
+      ->required()
+      ->check(CLI::ExistingFile);
+  app.add_option("--output,-o,2",outputName, "the output base name.", true );
+  app.add_option("--invertNormal,-n", invertNormal, "invert normal to apply accumulation.");
+  app.add_option("--radiusCompare,-r", radiusCompare, "reference Radius to apply comparisons.", true);
+  app.add_option("--radiusComputeAcc,-d", radiusComputeAcc, "distance (d_{acc}) used to compute the accumulation.", true);
+  app.add_option("--confidenceTh,-c", confidenceTh, "threshold confidence image to apply stat.", true);
+  app.add_option("--accumulationTh,-a", confidenceTh, "threshold accumulation image to apply stat.", true);
+    
+  app.get_formatter()->column_width(40);
+  CLI11_PARSE(app, argc, argv);
+  // END parse command line using CLI ----------------------------------------------
+
+
   
   DGtal::trace.info() << "------------------------------------ "<< std::endl;
   DGtal::trace.info() << "Step 1: Reading input mesh ... ";
@@ -120,8 +93,6 @@ int main(int argc, char *const *argv)
   acc.initFromMesh(aMesh, invertNormal);
   DGtal::trace.info() << " [done] " << std::endl;  
   DGtal::trace.info() << "------------------------------------ "<< std::endl;  
-  
-
   
 
   // 3) Compute accumulation and confidence
@@ -155,7 +126,6 @@ int main(int argc, char *const *argv)
       double valAcc = imageAccumulation(p)/maxAccumulation;
       double minDistance = imageRadiusAcc(p);
       
-      
       if(valConf > confidenceTh )
       {
         double errorConf = (radiusCompare-minDistance)/radiusCompare;
@@ -173,7 +143,6 @@ int main(int argc, char *const *argv)
         }
       }
     }
-  
   
   stringstream ssConfResName;
   ssConfResName<< outputName << "Confidence.dat";
@@ -203,10 +172,7 @@ int main(int argc, char *const *argv)
   outAcc.close();
   outConf.close();
   
-  
-  
   DGtal::trace.info() << "[Done]" <<std::endl;
-
   return 0;
 }
 
